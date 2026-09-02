bf2ec439a8fcdc25d534b87a07c3abfcb8fb0f8b
Jacob Farnsworth <jacoblewis.farnsworth@ntig.se>
2026-09-02 08:45:51 +0200

    Change background color

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ TEXT_COLOR = (255, 255, 255)
 PAPER_COLOR = (255, 255, 255)
 PADDLE_COLOR = (128, 128, 128)
 TEARS_COLOR = (0, 0, 255)
-BACKGROUND_COLOR = (22, 22, 22)
+BACKGROUND_COLOR = (0, 0, 0)
 
 # Clock to control frame rate
 clock = pygame.time.Clock()
